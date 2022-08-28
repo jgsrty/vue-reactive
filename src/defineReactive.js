38212cd46251376f89c86ada6aa0730285615846
RtyXmd <573850993@qq.com>
2022-08-28 22:30:23 +0800

    封装defineReactive函数

--- src/defineReactive.js
+++ src/defineReactive.js
@@ -1,21 +1,35 @@
-const obj = {}
+/**
+ * 定义响应式-完成数据的劫持
+ * @param {object} data: 需要添加属性的对象
+ * @param {*} key: 属性名
+ * @param {*} value: 属性值
+ */
 
-let temp = ''
-// 向obj添加一个属性
-Object.defineProperty(obj,'a',{
-  enumerable:true, //可以被枚举（for遍历）
-  configurable: true, //可以被配置（删除）
-  get() {
-    // 当a属性被调用（访问）时，自动调用返回结果作为属性的值
-    return temp
-  },
-  set(newValue) {
-    // 当a属性被设置时，会自动调用，接受新的值（设置的值）
-    console.log('设置了a')
-    temp = newValue
-  }
-})
+function defineReactive(data, key, val) {
+  // 向obj添加一个属性
+  Object.defineProperty(data, key, {
+    enumerable: true, //可以被枚举（for遍历）
+    configurable: true, //可以被配置（删除）
+    get() {
+      // 当a属性被调用（访问）时，自动调用返回结果作为属性的值
+      console.log(`访问了${key}`);
+      return val;
+    },
+    set(newValue) {
+      // 当a属性被设置时，会自动调用，接受新的值（设置的值）
+      console.log(`设置了${key}的属性：${key}=${newValue}`);
+      if (val === newValue) return;
+      val = newValue;
+    },
+  });
+}
 
-console.log(obj.a)
-obj.a = 2000
-console.log(obj.a)
+const obj = {};
+
+defineReactive(obj, "a", 100);
+console.log(obj.a);
+obj.a = 200;
+console.log(obj.a);
+
+defineReactive(obj, "b", 1111);
+console.log(obj.b);
